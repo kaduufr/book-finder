649d8b4370fd63322fa6ee93de42330ebed7a079
Carlos Eduardo <byedu1234@hotmail.com>
2020-04-02 23:55:50 -0300

    Application finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { FaSearch } from 'react-icons/fa'
+import axios from 'axios'
+
+import Teste from './assets/loading.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+  const [livros, setLivros] = useState([])
+  const [title, setTitle] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function handleBook() {
+    if (title === '') {
+      alert('No results')
+      return
+    }
+
+    setLoading(true)
+    const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title.split(' ').join('+')}`)
+    setLivros(res.data.items)
+    setLoading(false)
+  }
+
+  function formatDescription(description) {
+    return `${description.substring(0, 1).toUpperCase()}${description.substring(1, 100).toLowerCase()} ...`
+  }
+
+  function listLivros() {
+    return livros && livros.map(livro => (
+      <a className="item" key={livro.id} href={livro.volumeInfo.infoLink} target="blank" >
+          <div className="item-title">
+            <h3>{livro.volumeInfo.title}</h3>
+          </div>
+          <div className="item-image">
+            <img src={livro.volumeInfo.imageLinks.thumbnail} alt=""/>
+          </div>
+          <p className="item-author">
+            <b>Author: </b> {livro.volumeInfo.authors.join(', ')}
+          </p>
+          <p className="item-published">
+            <b>Published: </b>{livro.volumeInfo.publishedDate}
+          </p>
+          <p className="item-description">
+            {livro.volumeInfo.description && formatDescription(livro.volumeInfo.description) }
+          </p>
         </a>
-      </header>
+    ))
+  }
+
+
+  return (
+    <div className="app">
+      <div className="header">
+        <div className="title">
+          Book Finder
+        </div>
+        <div className="box-inputs"  >
+          <input type="text" placeholder="Digite o nome do livro/author" value={title} onChange={e => setTitle(e.target.value)} />
+          <button onClick={handleBook} > <FaSearch></FaSearch> </button>
+        </div>
+      </div>
+      <div className="content">
+        { loading  && <img src={Teste} alt=""/> }
+        {listLivros()}
+      </div>
     </div>
   );
 }
